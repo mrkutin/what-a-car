4e36c63fd82bd8ca35540c7509cfac34492b69bd
Mr. Kutin <serge.kutin@gmail.com>
2023-01-27 15:27:33 +0300

    storage server does not connect to redis at 127.0.0.1

--- services/storage/index.mjs
+++ services/storage/index.mjs
@@ -1,33 +1,43 @@
+const MONGO_HOST = process.env.MONGO_HOST || 'mongodb://root:root@0.0.0.0:27017'
+const REDIS_HOST = process.env.REDIS_HOST || 'redis://0.0.0.0:6379'
+
 import {createClient} from 'redis'
 
 const redisClient = createClient()
 const redisSubscriber = redisClient.duplicate()
 
 import {MongoClient} from 'mongodb'
-
-const url = `mongodb://root:root@localhost:27017`
-const mongoClient = new MongoClient(url)
-const mongoDbName = 'what-a-car'
+const mongoClient = new MongoClient(MONGO_HOST)
 await mongoClient.connect()
-const mongoDb = mongoClient.db(mongoDbName)
+const mongoDb = mongoClient.db('what-a-car')
 const collection = mongoDb.collection('plates')
+await collection.createIndex({plate: 1})
 
-await redisClient.connect()
+await redisClient.connect({
+    url: REDIS_HOST
+})
 redisClient.on('error', (err) => console.log('Redis Client Error', err))
-await redisSubscriber.connect()
+await redisSubscriber.connect({
+    url: REDIS_HOST
+})
 redisSubscriber.on('error', (err) => console.log('Redis Subscriber Error', err))
 
 await redisSubscriber.pSubscribe('__keyevent*__:*', async (key) => {
-    console.time('upsert')
+    // console.time('upsert')
     const [service, plate] = key.split(':')
     const value = await redisClient.json.get(key)
     const foundResult = await collection.findOne({plate})
 
     const now = new Date()
-    if(!foundResult){
+    if (!foundResult) {
         await collection.insertOne({plate, services: {[service]: {...value, updatedAt: now}}, createdBy: now})
     } else {
-        await collection.updateOne({plate}, {$set: {[`services.${service}`]: {...value, updatedAt: now}, updatedAt: now}})
+        await collection.updateOne({plate}, {
+            $set: {
+                [`services.${service}`]: {...value, updatedAt: now},
+                updatedAt: now
+            }
+        })
     }
-    console.timeEnd('upsert')
+    // console.timeEnd('upsert')
 })
