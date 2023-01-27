2344137233ff9f308fdd8043f01fff146bedb95f
Mr. Kutin <serge.kutin@gmail.com>
2023-01-27 14:56:22 +0300

    storage service

--- services/storage/index.mjs
+++ services/storage/index.mjs
@@ -7,27 +7,27 @@ import {MongoClient} from 'mongodb'
 
 const url = `mongodb://root:root@localhost:27017`
 const mongoClient = new MongoClient(url)
-const mongoDbName = 'auto-info'
+const mongoDbName = 'what-a-car'
 await mongoClient.connect()
 const mongoDb = mongoClient.db(mongoDbName)
-const collection = mongoDb.collection('cars')
+const collection = mongoDb.collection('plates')
 
 await redisClient.connect()
 redisClient.on('error', (err) => console.log('Redis Client Error', err))
 await redisSubscriber.connect()
 redisSubscriber.on('error', (err) => console.log('Redis Subscriber Error', err))
 
-await redisSubscriber.pSubscribe('__keyevent*__:*', async (vin) => {
+await redisSubscriber.pSubscribe('__keyevent*__:*', async (key) => {
     console.time('upsert')
-    const json = await redisClient.json.get(vin)
+    const [service, plate] = key.split(':')
+    const value = await redisClient.json.get(key)
+    const foundResult = await collection.findOne({plate})
 
-    const findResult = await collection.findOne({vin})
-    if(!findResult){
-        await collection.insertOne({vin, ...json, createdAt: new Date()})
+    const now = new Date()
+    if(!foundResult){
+        await collection.insertOne({plate, services: {[service]: {...value, updatedAt: now}}, createdBy: now})
     } else {
-        await collection.updateOne({vin}, {$set: {...json, updatedAt: new Date()}})
+        await collection.updateOne({plate}, {$set: {[`services.${service}`]: {...value, updatedAt: now}, updatedAt: now}})
     }
-
-    console.log(json)
     console.timeEnd('upsert')
 })
