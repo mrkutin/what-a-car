f91c79feb9592cef5aa4716fb189d7245bc6f3a5
Serge Kutin <skutin@prosv.ru>
2023-01-27 20:10:23 +0300

    tg bot formatting

--- services/telegram-bot/index.mjs
+++ services/telegram-bot/index.mjs
@@ -1,4 +1,7 @@
-const {TELEGRAM_BOT_TOKEN} = process.env
+import axios from "axios";
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
+const WHAT_A_CAR_API_HOST = process.env.WHAT_A_CAR_API_HOST || 'http://0.0.0.0:3000'
 const letterMap = {
     'A': 'А',
     'B': 'В',
@@ -29,38 +32,52 @@ bot.on('text', async (ctx) => {
     const {from, text} = message
     const {is_bot} = from
 
-    if (validateVin(text)) {
-        return ctx.reply('Это валидный VIN: ' + text)
-    }
-
-    const normalizedText = text.toUpperCase().split('').map(letter => letterMap[letter] || letter).join('')
-
-    if (normalizedText.match(/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui)) {
-        return ctx.reply('Это номер обычного автомобиля: ' + normalizedText)
-    }
-
-    if (normalizedText.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui)) {
-        return ctx.reply('Это номер такси: ' + normalizedText)
-    }
-
-    if (normalizedText.match(/^[АВЕКМНОРСТУХ]{2}\d{4}(?<!0000)\d{2,3}$/ui)) {
-        return ctx.reply('Это номер прицепа: ' + normalizedText)
-    }
-
-    if (normalizedText.match(/^\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui)) {
-        return ctx.reply('Это номер мотоцикла: ', normalizedText)
-    }
-
-    if (normalizedText.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ]\d{2,3}$/ui)) {
-        return ctx.reply('Это транзитный номер: ' + normalizedText)
-    }
-
-    if (normalizedText.match(/^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui)) {
-        return ctx.reply('Это выездной номер: ' + normalizedText)
-    }
-
     if (is_bot) {
         return ctx.reply('Опять бот! А хочется простого человеческого общения...')
+    }
+
+    const plate = text.toUpperCase().split('').map(letter => letterMap[letter] || letter).join('')
+
+    if (
+        plate.match(/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui)
+        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //такси
+        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{4}(?<!0000)\d{2,3}$/ui) //прицеп
+        || plate.match(/^\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui) //мотоцикл
+        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ]\d{2,3}$/ui) //транзит
+        || plate.match(/^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //выездной
+    ) {
+        try {
+            const res = await axios.get(`${WHAT_A_CAR_API_HOST}/api/plates/${plate}`)
+            if (!res.data) {
+                return ctx.reply('Информация временно недоступна')
+            }
+            ctx.reply('Вот, что удалось найти:')
+
+            const {sravni, gibdd, autoins} = res.data
+
+            await ctx.reply(`Гос. номер: ${plate}`)
+            await ctx.reply(`VIN: ${gibdd.vehicle?.vin}`)
+            await ctx.reply(`Название: ${gibdd.vehicle?.model || `${sravni.brand?.name || ''} ${sravni.model?.name || ''}`}`)
+            await ctx.reply(`Цвет: ${gibdd.vehicle?.color || ''}`)
+            await ctx.reply(`Год выпуска: ${gibdd.vehicle?.year || ''}`)
+            await ctx.reply(`Объем двигателя: ${gibdd.vehicle?.engineVolume || ''} куб. см`)
+            await ctx.reply(`Мощность: ${gibdd.vehicle?.powerHp || ''} л.с.`)
+            await ctx.reply(`Собственников по ПТС: ${gibdd.ownershipPeriods?.length || ''}`)
+            gibdd.accidents?.forEach(async accident => {
+                await ctx.reply(`${accident.AccidentDateTime}, ${accident.AccidentPlace}, ${accident.AccidentType}, кол-во участников: ${accident.VehicleAmount}`)
+            })
+
+
+
+            return
+        } catch (e) {
+            console.log(e.message)
+            return ctx.reply('Информация временно недоступна')
+        }
+    }
+
+    if (validateVin(text)) {
+        return ctx.reply('Это валидный VIN: ' + text)
     }
 
     return ctx.reply('Это не похоже ни на номер, ни на VIN, попробуй еще раз')
