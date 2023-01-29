3d91a8f1e7d9dbea465c7400bf5e5c990607e558
Serge Kutin <skutin@prosv.ru>
2023-01-29 16:17:17 +0300

    events started

--- services/telegram-bot/index.mjs
+++ services/telegram-bot/index.mjs
@@ -1,8 +1,6 @@
-import axios from "axios";
-
+const REDIS_HOST = process.env.REDIS_HOST || 'redis://0.0.0.0:6379'
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
-const WHAT_A_CAR_API_HOST = process.env.WHAT_A_CAR_API_HOST || 'http://0.0.0.0:3000'
-const letterMap = {
+const en2ruMap = {
     'A': 'А',
     'B': 'В',
     'E': 'Е',
@@ -16,9 +14,30 @@ const letterMap = {
     'Y': 'У',
     'X': 'Х'
 }
+const ru2enMap = {
+    'А': 'A',
+    'В': 'B',
+    'Е': 'E',
+    'К': 'K',
+    'М': 'M',
+    'Н': 'H',
+    'О': 'O',
+    'Р': 'P',
+    'С': 'C',
+    'Т': 'T',
+    'У': 'Y',
+    'Х': 'X'
+}
+
+import {createClient} from 'redis'
+const redisClient = createClient({url: REDIS_HOST})
+const redisSubscriber = redisClient.duplicate()
+redisClient.on('error', (err) => console.log('Redis Client Error', err))
+await redisClient.connect()
+redisSubscriber.on('error', (err) => console.log('Redis Subscriber Error', err))
+await redisSubscriber.connect()
 
 import {Telegraf} from 'telegraf'
-
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
 
 //todo use split() in a new service "decodeVin"
@@ -32,46 +51,48 @@ bot.on('text', async (ctx) => {
     const {from, text} = message
     const {is_bot} = from
 
-
     if (is_bot) {
         return ctx.reply('Опять бот! А хочется простого человеческого общения...')
     }
 
-    const plate = text.toUpperCase().split('').map(letter => letterMap[letter] || letter).join('')
+    const id = text.toUpperCase()
+    const plate = id.split('').map(letter => en2ruMap[letter] || letter).join('')
+    const vin = id.split('').map(letter => ru2enMap[letter] || letter).join('')
 
     if (
-        plate.match(/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui)
-        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //такси
-        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{4}(?<!0000)\d{2,3}$/ui) //прицеп
-        || plate.match(/^\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui) //мотоцикл
-        || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ]\d{2,3}$/ui) //транзит
-        || plate.match(/^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //выездной
+        plate.match(/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui) //обычный номер
+        // || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //такси
+        // || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{4}(?<!0000)\d{2,3}$/ui) //прицеп
+        // || plate.match(/^\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui) //мотоцикл
+        // || plate.match(/^[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ]\d{2,3}$/ui) //транзит
+        // || plate.match(/^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}$/ui) //выездной
     ) {
-        try {
-            const res = await axios.get(`${WHAT_A_CAR_API_HOST}/api/${plate}`)
-            if (!res.data) {
-                return ctx.reply('Информация не найдена')
-            }
-
-            for (const line of res.data){
-                await ctx.reply(line)
-            }
-
-            return
-        } catch (e) {
-            console.log(e.message)
-            return ctx.reply('Информация временно недоступна')
+        const checkRequest = await redisClient.json.get(`tg:${chat_id}-${plate}`)
+        if(!checkRequest){
+            await redisClient.json.set(`tg:${chat_id}-${plate}`, '$', {})
         }
+    } else if (vin.match(/^[A-Z0-9]{17}$/g)) {
+        const checkRequest = await redisClient.json.get(`tg:${chat_id}-${vin}`)
+        if(!checkRequest){
+            await redisClient.json.set(`tg:${chat_id}-${vin}`, '$', {})
+        }
+    } else {
+        ctx.reply('Это не похоже ни на номер, ни на VIN')
     }
-
-    if (validateVin(text)) {
-        return ctx.reply('Это валидный VIN: ' + text)
-    }
-
-    return ctx.reply('Это не похоже на номер')
 })
 
-bot.launch()
+await bot.launch()
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+await redisSubscriber.pSubscribe('__keyevent*__:*', async (key) => {
+    // console.time('upsert')
+    const [service, id] = key.split(':')
+    const value = await redisClient.json.get(key)
+
+    //check if query has been made
+    const checkRequest = await redisClient.json.get(`tg:${chat_id}-${id}`)//todo gey chat ids somehow here
+
+    // console.timeEnd('upsert')
+})
