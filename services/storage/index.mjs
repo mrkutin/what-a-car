621490c3d25d16b14b99bdfec72901a4d91555a7
Mr. Kutin <serge.kutin@gmail.com>
2023-02-04 15:22:29 +0300

    storage

--- services/storage/index.mjs
+++ services/storage/index.mjs
@@ -6,8 +6,10 @@ import {MongoClient} from 'mongodb'
 const mongoClient = new MongoClient(MONGO_HOST)
 await mongoClient.connect()
 const mongoDb = mongoClient.db('what-a-car')
-const collection = mongoDb.collection('plates')
-await collection.createIndex({plate: 1})
+const plates = mongoDb.collection('plates')
+await plates.createIndex({plate: 1})
+const chats = mongoDb.collection('chats')
+await chats.createIndex({chat_id: 1})
 
 import Redis from 'ioredis'
 
@@ -32,6 +34,11 @@ try {
     await redisSub.xgroup('CREATE', 'stream:fines:resolved', 'storage', '$', 'MKSTREAM')
 } catch (e) {
     console.log('Group "storage" already exists in stream:fines:resolved, skipping')
+}
+try {
+    await redisSub.xgroup('CREATE', 'stream:plate:requested', 'storage', '$', 'MKSTREAM')
+} catch (e) {
+    console.log('Group "storage" already exists in sstream:plate:requested, skipping')
 }
 
 const makeId = length => {
@@ -68,28 +75,58 @@ async function listenForMessages() {
         'stream:autoins:resolved',
         'stream:gibdd:resolved',
         'stream:fines:resolved',
+        'stream:plate:requested',
+        '>',
         '>',
         '>',
         '>',
         '>'
     )
 
-    const flatMessages = results.reduce((acc, result) => {
+    const flatMessagesPlateRequested = results
+        .filter(([stream]) => stream === 'stream:plate:requested')
+        .reduce((acc, result) => {
+            return acc.concat(result[1])//messages
+        }, [])
+
+    for (const message of flatMessagesPlateRequested) {
+        const {plate, chat_id, user_id, user_name, user_first_name, user_last_name, user_language_code} = flatArrayToObject(message[1])
+
+        const foundResult = await chats.findOne({chat_id: parseInt(chat_id)})
+        const now = new Date()
+
+        if (!foundResult) {
+            await chats.insertOne({chat_id: parseInt(chat_id), user_id: parseInt(user_id), user_name, user_first_name, user_last_name, user_language_code, plates: [plate], createdBy: now})
+        } else {
+            await chats.updateOne({chat_id: parseInt(chat_id)}, {
+                $addToSet: {
+                    plates: plate
+                },
+                $set: {
+                    user_id: parseInt(user_id), user_name, user_first_name, user_last_name, user_language_code, updatedAt: now
+                }
+            })
+        }
+    }
+
+    const flatMessagesResolved = results
+        .filter(([stream]) => stream !== 'stream:plate:requested')
+        .reduce((acc, result) => {
         return acc.concat(result[1])//messages
     }, [])
 
-    for (const message of flatMessages) {
+    for (const message of flatMessagesResolved) {
         const {key, chat_id, plate} = flatArrayToObject(message[1])
         const [service,] = key.split(':')
         let serviceObj = JSON.parse(await redisPub.call('JSON.GET', key))
 
-        const foundResult = await collection.findOne({plate})
+        const foundResult = await plates.findOne({plate})
         const now = new Date()
 
         if (!foundResult) {
-            await collection.insertOne({plate, chats: [parseInt(chat_id)], services: {[service]: {...serviceObj, updatedAt: now}}, createdBy: now})
+            await plates.insertOne({plate, chats: [parseInt(chat_id)], services: {[service]: {...serviceObj, updatedAt: now}}, createdBy: now})
         } else {
-            await collection.updateOne({plate}, {
+            await plates.updateOne({plate}, {
                 $addToSet: {
                     chats: parseInt(chat_id)
                 },
@@ -115,13 +152,13 @@ listenForMessages()
 //     // console.time('upsert')
 //     const [service, plate] = key.split(':')
 //     const value = await redisClient.json.get(key)
-//     const foundResult = await collection.findOne({plate})
+//     const foundResult = await plates.findOne({plate})
 //
 //     const now = new Date()
 //     if (!foundResult) {
-//         await collection.insertOne({plate, services: {[service]: {...value, updatedAt: now}}, createdBy: now})
+//         await plates.insertOne({plate, services: {[service]: {...value, updatedAt: now}}, createdBy: now})
 //     } else {
-//         await collection.updateOne({plate}, {
+//         await plates.updateOne({plate}, {
 //             $set: {
 //                 [`services.${service}`]: {...value, updatedAt: now},
 //                 updatedAt: now
