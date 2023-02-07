ef5eb9e85a74499569d1f87540bc7871bbeb8851
Serge Kutin <skutin@prosv.ru>
2023-02-07 13:48:16 +0300

    undefined in tg

--- services/telegram-bot/index.mjs
+++ services/telegram-bot/index.mjs
@@ -182,32 +182,32 @@ async function listenForMessages() {
             case 'autoins':
                 if (serviceObj.policyId) {
                     await bot.telegram.sendMessage(chat_id, '<b>ОСАГО</b>', {parse_mode: 'HTML'})
-                    await bot.telegram.sendMessage(chat_id, `Номер: ${serviceObj.licensePlate}`)
-                    await bot.telegram.sendMessage(chat_id, `VIN: ${serviceObj.vin}`)
-                    await bot.telegram.sendMessage(chat_id, `Автомобиль: ${serviceObj.makeAndModel}`)
-                    await bot.telegram.sendMessage(chat_id, `Мощность: ${serviceObj.powerHp} л.с.`)
-                    await bot.telegram.sendMessage(chat_id, `Полис ОСАГО: ${serviceObj.policyId} ${serviceObj.company} ${serviceObj.status}`)
-                    await bot.telegram.sendMessage(chat_id, `Цель использования: ${serviceObj.usingPurpose}`)
-                    await bot.telegram.sendMessage(chat_id, `${serviceObj.hasRestrictions}`)
-                    await bot.telegram.sendMessage(chat_id, `Собственник: ${serviceObj.vehicleOwner}`)
-                    await bot.telegram.sendMessage(chat_id, `Страхователь: ${serviceObj.policyHolder}`)
-                    await bot.telegram.sendMessage(chat_id, `КБМ: ${serviceObj.KBM}`)
-                    await bot.telegram.sendMessage(chat_id, `Регион: ${serviceObj.region}`)
-                    await bot.telegram.sendMessage(chat_id, `Страховая премия: ${serviceObj.premium}`)
+                    await bot.telegram.sendMessage(chat_id, `Номер: ${serviceObj.licensePlate || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `VIN: ${serviceObj.vin || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Автомобиль: ${serviceObj.makeAndModel || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Мощность: ${serviceObj.powerHp ? `${serviceObj.powerHp} л.с.` : ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Полис ОСАГО: ${serviceObj.policyId || ''} ${serviceObj.company || ''} ${serviceObj.status || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Цель использования: ${serviceObj.usingPurpose || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `${serviceObj.hasRestrictions || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Собственник: ${serviceObj.vehicleOwner || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Страхователь: ${serviceObj.policyHolder || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `КБМ: ${serviceObj.KBM || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Регион: ${serviceObj.region || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Страховая премия: ${serviceObj.premium || ''}`)
                 }
                 break
             case 'gibdd':
                 if (serviceObj.vehicle) {
                     await bot.telegram.sendMessage(chat_id, '<b>АВТОМОБИЛЬ</b>', {parse_mode: 'HTML'})
-                    await bot.telegram.sendMessage(chat_id, `VIN: ${serviceObj.vehicle.vin}`)
-                    await bot.telegram.sendMessage(chat_id, `Название: ${serviceObj.vehicle.model}`)
-                    await bot.telegram.sendMessage(chat_id, `Категория: ${serviceObj.vehicle.category}`)
-                    await bot.telegram.sendMessage(chat_id, `Цвет: ${serviceObj.vehicle.color}`)
-                    await bot.telegram.sendMessage(chat_id, `Год: ${serviceObj.vehicle.year}`)
-                    await bot.telegram.sendMessage(chat_id, `Объем двигателя: ${serviceObj.vehicle.engineVolume}`)
-                    await bot.telegram.sendMessage(chat_id, `Номер кузова: ${serviceObj.vehicle.bodyNumber}`)
-                    await bot.telegram.sendMessage(chat_id, `Номер двигателя: ${serviceObj.vehicle.engineNumber}`)
-                    await bot.telegram.sendMessage(chat_id, `Мощность двигателя: ${serviceObj.vehicle.powerHp} л.с.`)
+                    await bot.telegram.sendMessage(chat_id, `VIN: ${serviceObj.vehicle.vin || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Название: ${serviceObj.vehicle.model || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Категория: ${serviceObj.vehicle.category || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Цвет: ${serviceObj.vehicle.color || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Год: ${serviceObj.vehicle.year || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Объем двигателя: ${serviceObj.vehicle.engineVolume || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Номер кузова: ${serviceObj.vehicle.bodyNumber || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Номер двигателя: ${serviceObj.vehicle.engineNumber || ''}`)
+                    await bot.telegram.sendMessage(chat_id, `Мощность двигателя: ${serviceObj.vehicle.powerHp ? `${serviceObj.vehicle.powerHp} л.с.` : ''}`)
                 }
 
                 if (serviceObj.ownershipPeriods?.length) {
@@ -220,7 +220,7 @@ async function listenForMessages() {
                 if (serviceObj.accidents?.length) {
                     await bot.telegram.sendMessage(chat_id, '<b>УЧАСТИЕ в ДТП</b>', {parse_mode: 'HTML'})
                     for (const accident of serviceObj.accidents) {
-                        await bot.telegram.sendMessage(chat_id, `${accident.AccidentDateTime} ${accident.AccidentPlace}, ${accident.AccidentType.toLowerCase()}, количество участников: ${accident.VehicleAmount}${accident?.DamagePoints?.length ? `, повреждения: ${accident?.DamagePoints.join(', ')}` : ''}`)
+                        await bot.telegram.sendMessage(chat_id, `${accident.AccidentDateTime || ''} ${accident.AccidentPlace || ''}, ${accident.AccidentType ? accident.AccidentType.toLowerCase() : ''}, количество участников: ${accident.VehicleAmount || ''}${accident?.DamagePoints?.length ? `, повреждения: ${accident?.DamagePoints.join(', ')}` : ''}`)
                     }
                 }
                 break
@@ -238,12 +238,12 @@ async function listenForMessages() {
                 await bot.telegram.sendMessage(chat_id, '<b>ДОКУМЕНТЫ</b>', {parse_mode: 'HTML'})
                 if (serviceObj.documents?.length) {
                     for(const document of serviceObj.documents){
-                        await bot.telegram.sendMessage(chat_id, `${document.type.name}: ${document.number} от ${document.date.substring(0, 10)}`)
+                        await bot.telegram.sendMessage(chat_id, `${document.type.name || ''}: ${document.number || ''} от ${document.date.substring(0, 10)}`)
                     }
                 }
                 if (serviceObj.identifiers?.length) {
                     for(const identifier of serviceObj.identifiers){
-                        await bot.telegram.sendMessage(chat_id, `${identifier.type.name}: ${identifier.number}`)
+                        await bot.telegram.sendMessage(chat_id, `${identifier.type.name || ''}: ${identifier.number || ''}`)
                     }
                 }
         }
